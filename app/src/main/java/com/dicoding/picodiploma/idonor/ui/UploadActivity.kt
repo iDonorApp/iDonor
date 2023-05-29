47db07bad313e4d08ceeaaec8fcae8000e4c5d56
kybezor <20102069@ittelkom-pwt.ac.id>
2023-05-29 13:12:23 +0700

    layout

---
+++ app/src/main/java/com/dicoding/picodiploma/idonor/ui/UploadActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.idonor.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.idonor.R
+
+class UploadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload)
+    }
+}
