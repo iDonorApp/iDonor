6a68418b94bb51137b34976ba6fdab057d3eaa7c
Hanief Fathul Bahri Ahmad <hanieffathulb03@gmail.com>
2023-06-06 17:19:32 +0700

    fix undifined update data

--- src/controller/profileController.js
+++ src/controller/profileController.js
@@ -33,45 +33,36 @@ const updateUserProfile = async (req, res) => {
   const { body } = req;
 
   try {
-    const existingProfile = await profileModel.updateProfile(body, iduser);
+    // Mendapatkan data pengguna berdasarkan ID
+    const [data] = await profileModel.getUserById(iduser);
 
-    // // if (!existingProfile) {
-    //   return res.status(404).json({ message: 'Profile not found' });
-    // // }
-
-    // Merge existing profile data with updated fields from the request body
-    const updatedProfile = { ...body };
-
-    // Check if all required properties have valid values
-    if (!updatedProfile.nama || !updatedProfile.golongan || !updatedProfile.jenis_kelamin) {
-      return res.status(400).json({ message: 'Invalid request. Missing required fields' });
+    // Memeriksa apakah data pengguna ditemukan
+    if (!data) {
+      return res.status(404).json({ message: 'Profile tidak ada' });
     }
 
-    // Check if the password field exists and update the password if it does
+    // Mengupdate password jika ada password baru yang diberikan
     if (body.password) {
-      // Generate a salt to be used for hashing
       const saltRounds = 10;
       const salt = await bcrypt.genSalt(saltRounds);
-
-      // Hash the new password
       const hashedPassword = await bcrypt.hash(body.password, salt);
-
-      // Update the password in the updated profile
-      updatedProfile.password = hashedPassword;
+      body.password = hashedPassword;
     }
 
-    await profileModel.updateProfile(updatedProfile, iduser);
+    // Mengupdate profil pengguna
+    await profileModel.updateProfile(body, iduser);
 
-    res.status(200).json({
+    const response = {
       message: 'Profile updated successfully',
-      data: updatedProfile,
-    });
+      data: body,
+    };
+
+    res.status(200).json(response);
   } catch (error) {
     console.error('Server error:', error);
     res.status(500).json({ message: 'Server error', serverMessage: error.message });
   }
 };
-
 
 
 module.exports = {
--- src/models/profileModel.js
+++ src/models/profileModel.js
@@ -1,26 +1,46 @@
 const dbpool = require('../config/database');
 
 const getUserById = (iduser) => {
-    const SQLQuery = `SELECT * FROM users WHERE id_users = '${iduser}'`;
-    return dbpool.execute(SQLQuery);
+  const SQLQuery = `SELECT * FROM users WHERE id_users = '${iduser}'`;
+  return dbpool.execute(SQLQuery);
 };
-
 
 const updateProfile = (body, iduser) => {
-    const SQLQuery = `UPDATE users SET 
-                        nama= '${body.nama}', 
-                        golongan= '${body.golongan}', 
-                        jenis_kelamin= '${body.jenis_kelamin}',
-                        tanggal_lahir= '${body.tanggal_lahir}',
-                        no_whatsapp= '${body.no_whatsapp}',
-                        alamat= '${body.alamat}',
-                        email= '${body.email}',
-                        password= '${body.password}'
-                        WHERE id_users = '${iduser}'`;
+  let SQLQuery = `UPDATE users SET`;
 
-    return dbpool.execute(SQLQuery);
+  // Memeriksa dan menambahkan kolom yang akan diupdate ke SQLQuery
+  if (body.nama) {
+    SQLQuery += ` nama = '${body.nama}',`;
+  }
+  if (body.golongan) {
+    SQLQuery += ` golongan = '${body.golongan}',`;
+  }
+  if (body.jenis_kelamin) {
+    SQLQuery += ` jenis_kelamin = '${body.jenis_kelamin}',`;
+  }
+  if (body.tanggal_lahir) {
+    SQLQuery += ` tanggal_lahir = '${body.tanggal_lahir}',`;
+  }
+  if (body.no_whatsapp) {
+    SQLQuery += ` no_whatsapp = '${body.no_whatsapp}',`;
+  }
+  if (body.alamat) {
+    SQLQuery += ` alamat = '${body.alamat}',`;
+  }
+  if (body.email) {
+    SQLQuery += ` email = '${body.email}',`;
+  }
+  if (body.password) {
+    SQLQuery += ` password = '${body.password}',`;
+  }
+
+  // Menghapus koma terakhir dari SQLQuery
+  SQLQuery = SQLQuery.slice(0, -1);
+
+  // Menambahkan WHERE clause untuk ID pengguna yang sesuai
+  SQLQuery += ` WHERE id_users = '${iduser}'`;
+
+  return dbpool.execute(SQLQuery);
 };
-
-
 
 module.exports = { getUserById, updateProfile };
